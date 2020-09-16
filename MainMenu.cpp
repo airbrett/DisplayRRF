52da656b1cc03e6206a081f4d0d5d1d7a773e458
airbrett <mixmasterbrett@gmail.com>
2020-09-15 22:29:50 -0600

    Populate Run Macros menu

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,6 +1,15 @@
 #include "Data.h"
 #include "Utility.h"
 
+extern "C"
+{
+#include "json.h"
+}
+
+extern int MakeRequest(PGM_P Req, char* Resp, const int Len);
+
+static bool ParseM20(const char* JSON, const int BytesRead);
+
 void DrawMenu()
 {
   char EncPos = Enc.read()/4;
@@ -22,6 +31,7 @@ void DrawMenu()
         break;
       case 2:
         gData.CurrentPage = PG_RUNMACRO;
+        gData.P.RM.FileArray = NULL;
         break;
     }
   }
@@ -57,14 +67,67 @@ void DrawMacrosMenu()
       gData.CurrentPage = PG_MENU1;
   }
 
+  if (!gData.P.RM.FileArray)
+  {
+    const int BytesRead = MakeRequest(PSTR("M20 S2 P\"0:/macros\""), SerialBuffer, sizeof(SerialBuffer));
+    
+    if (BytesRead)
+    {
+      if (!ParseM20(SerialBuffer, BytesRead))
+        DEBUG_PRINT_P("Bad parse");
+    }
+  }
+
   PGM_P Text = PSTR("Run Macro");
   u8g2.firstPage();
   do
   {
     DrawStrP(u8g2.getDisplayWidth()/2-StrWidthP(Text)/2, 7, Text);
-    DrawStrP(5,14,PSTR("Back"));
+    u8g2.drawLine(0,8,u8g2.getDisplayWidth(),8);
+    DrawStrP(5,16,PSTR("Back"));
 
-    DrawStrP(0,7*EncPos+14,PSTR(">"));
+    DrawStrP(0,7*EncPos+16,PSTR(">"));
+
+    if (gData.P.RM.FileArray)
+    {
+      char* astate;
+      unsigned char v1type;
+      char* v1begin;
+      int v1len;
+      unsigned char Y = 23;
+      
+      char result = json_arr(&astate, gData.P.RM.FileArray, &v1type, &v1begin, &v1len);
+      
+      while (result > 0)
+      {
+        DrawStrJ(5, Y, v1begin, v1len);
+        Y += 7;
+        
+        result = json_arr(&astate, NULL, &v1type, &v1begin, &v1len);
+      }
+    }
   }
   while (u8g2.nextPage());
+}
+
+bool ParseM20(const char* JSON, const int BytesRead)
+{
+  char* jstate;
+  unsigned char v1type;
+  char* v1begin;
+  int v1len;
+  unsigned char v2type;
+  char* v2begin;
+  int v2len;
+  char result = json_obj(&jstate, JSON, &v1type, &v1begin, &v1len, &v2type, &v2begin, &v2len);
+
+  while (result > 0)
+  {
+    if (strcmpJP(PSTR("files"), v1begin, v1len) == 0)
+      gData.P.RM.FileArray = v2begin;
+    
+    result = json_obj(&jstate, NULL, &v1type, &v1begin, &v1len, &v2type, &v2begin, &v2len);
+  }
+
+  return true;
 }
--- Utility.h
+++ Utility.h
@@ -2,6 +2,8 @@
 
 void DrawStrP(PGM_P Str);
 void DrawStrP(const int x, const int y, PGM_P Str);
+void DrawStrJ(const char* Str, int Len);
+void DrawStrJ(const int x, const int y, const char* Str, int Len);
 unsigned int StrWidthP(PGM_P Str);
 char* PrintFloat(char* Str, unsigned char precision, bool padminus);
 char strcmpJP(PGM_P str1, char* str2, const int str2len);
